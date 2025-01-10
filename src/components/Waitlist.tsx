33407fadc4f17e00b628355ee7be0a5a166c017d
subham-proj <3133ss32@gmail.com>
2025-01-10 06:15:49 +0530

    firebase for auth added

---
+++ src/components/Waitlist.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+
+import { Alert, AlertTitle } from "@/components/ui/alert";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+export default function Waitlist(params: any) {
+  const [userData, setUserData] = useState<User>({ name: "", email: "" });
+  const [error, setError] = useState("");
+  const [isOpen, setIsOpen] = useState(false); // State to control the dialog
+  const { toast } = useToast();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setUserData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = () => {
+    if (userData.name === "" || userData.email === "") {
+      setError("Fill all the inputs");
+      return;
+    }
+    if (!validateEmail(userData.email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    toast({
+      title: "Awesome! You're on the waitlist 🎉",
+      description:
+        "You will be the first one to use the product when we launch",
+    });
+
+    setIsOpen(false); // Close the dialog
+  };
+
+  useEffect(() => {
+    if (error !== "") {
+      const timer = setTimeout(() => {
+        setError("");
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button onClick={() => setIsOpen(true)}>Join Waitlist</Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="font-thin">Join our waitlist now</DialogTitle>
+        </DialogHeader>
+        <DialogDescription></DialogDescription>
+        {error && (
+          <Alert variant="destructive">
+            <AlertTitle>{error}</AlertTitle>
+          </Alert>
+        )}
+
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              value={userData.name}
+              onChange={handleChange}
+              className="col-span-3"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="email"
+              value={userData.email}
+              onChange={handleChange}
+              className="col-span-3"
+              placeholder="Enter your email"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="button" onClick={handleSubmit}>
+            Join now
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
